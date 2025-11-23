c250d37f308b3d24ccc29211f36f9dd80d56c784
Husen Efendi <husenef@macbooks-MacBook-Pro.local>
2025-11-23 11:49:10 +0700

    feat(mail): preparet mail template for user and admin

---
+++ resources/views/emails/admin-new-user.blade.php
@@ -0,0 +1,5 @@
+<h1>New User Registered</h1>
+<p>Name: {{ $user->name }}</p>
+<p>Email: {{ $user->email }}</p>
+<p>Role: {{ $user->role }}</p>
+<p>Created At: {{ $user->created_at }}</p>
---
+++ resources/views/emails/user-welcome.blade.php
@@ -0,0 +1,4 @@
+<h1>Welcome, {{ $user->name }}!</h1>
+<p>Your account has been successfully created.</p>
+<p>Email: {{ $user->email }}</p>
+<p>Thank you for joining us.</p>
